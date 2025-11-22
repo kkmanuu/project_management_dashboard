f91bf90e2b1453e95530e1c978441ab21cd439af
kkmanuu <kipngenoemmanuel479@gmail.com>
2025-11-23 02:48:37 +0300

    feat: create cn utility for combining and merging class names

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,9 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+/**
+ * Combine class names and merge Tailwind classes.
+ */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-
