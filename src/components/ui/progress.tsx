2732f11ffe2794944486b7c9b370457ca7da2124
kkmanuu <kipngenoemmanuel479@gmail.com>
2025-11-27 02:09:32 +0300

    style: implement progress bar with smooth width transitions and custom styling

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -3,6 +3,7 @@ import * as ProgressPrimitive from "@radix-ui/react-progress";
 
 import { cn } from "@/lib/utils";
 
+// Progress bar component wrapper
 const Progress = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
@@ -12,12 +13,14 @@ const Progress = React.forwardRef<
     className={cn("relative h-4 w-full overflow-hidden rounded-full bg-secondary", className)}
     {...props}
   >
+    {/* Filled portion of the progress bar */}
     <ProgressPrimitive.Indicator
       className="h-full w-full flex-1 bg-primary transition-all"
       style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
     />
   </ProgressPrimitive.Root>
 ));
+
 Progress.displayName = ProgressPrimitive.Root.displayName;
 
 export { Progress };
